041111912490d77d53ed45346ad72dcf5a3ca2b2
SamuelsonPajeu <samuelson.pajeu@gmail.com>
2023-11-16 20:59:39 -0300

    chore: update tilt

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { React, useRef, useEffect } from "react";
+import { React, useState, useRef, useEffect } from "react";
 import './App.css';
 
 // import FrontCard from './components/frontCard/FrontCard.js';
 import BackCard from './components/backCard/BackCard.js';
 
 
-const applyScaling = (scaledContent) => {
+const getScaling = (scaledContent) => {
   scaledContent.style.transform = 'scale(1.25, 1.25)';
 
   const { width: cw, height: ch } = scaledContent.getBoundingClientRect();
@@ -15,70 +15,87 @@ const applyScaling = (scaledContent) => {
   const scaleAmtY = scaleAmtX;
 
   const scale = `scale(${scaleAmtX}, ${scaleAmtY})`;
-  scaledContent.settings.scale = scale;
 
-  scaledContent.style.transform = scale;
+  return scale
+}
+
+const applyScaling = (scaledContent) => {
+  scaledContent.style.transform = getScaling(scaledContent);
 };
 
-const applyTilt = (tiltedContent, event) => {
+const applyTilt = (tiltedContent, mouseEvent, lightComponent) => {
+  const innerX = mouseEvent.clientX - tiltedContent.offsetLeft;
+  const innerY = mouseEvent.clientY - tiltedContent.offsetTop;
+  
+  lightComponent.style.left = innerX + "px";
+  lightComponent.style.top = innerY + "px";
 
-  const settings = {
-    reverse: 0,        // Reverse tilt: 1, 0
-    max: 18,           // Max tilt: 35
-    axis: "",          // Limit axis. "y", "x"
-  };
+  const x = tiltedContent.offsetWidth / 2;
+  const y = tiltedContent.offsetHeight / 2;
 
-  const element = tiltedContent.getBoundingClientRect();
-  const x = Math.min(1, Math.max(0, (event.clientX - element.left) / element.width));
-  const y = Math.min(1, Math.max(0, (event.clientY - element.top) / element.height));
-  const reverse = settings.reverse ? -1 : 1;
-  const tiltX = reverse * (settings.max / 2 - x * settings.max);
-  const tiltY = reverse * (y * settings.max - settings.max / 2);
+  const intensityX = 8;
+  const intensityY = 3;
+
+  let convertX = ((innerX - x) * intensityX / x);
+  let convertY = ((innerY - y) * intensityY / y);
 
   tiltedContent.style.transform = `
-    rotateX(${settings.axis === "x" ? 0 : tiltY}deg)
-    rotateY(${settings.axis === "y" ? 0 : tiltX}deg)
-    ${tiltedContent.settings.scale}
+  rotateY(${convertX * -1}deg)
+  rotateX(${convertY}deg)
+  ${getScaling(tiltedContent)}
   `
 
 }
 
 function App() {
   const scaledContent = useRef();
-  scaledContent.settings = {
-    'scale' : 1
+  const lightComponent = useRef();
+  const [light, setLight] = useState(false);
+
+  function handleTilt(event){
+    if (lightComponent.current){
+      applyTilt(scaledContent.current, event, lightComponent.current);
+    }
+  }
+
+  function handleMouseOut(){
+    if(scaledContent.current){
+      scaledContent.current.style.transform = `
+      rotateY(0deg)
+      ${getScaling(scaledContent.current)}
+      `
+    }
   }
 
   useEffect(() => {
-
     function handleResize(){
-      if(scaledContent.current) {
-        applyScaling(scaledContent.current);
-      }
+      applyScaling(scaledContent.current);
     }
 
-    function handleTilt(event){
-      if(scaledContent.current) {
-        applyTilt(scaledContent.current, event);
-      }
-    }
+    if(scaledContent.current) {
+      window.addEventListener("resize", handleResize);
 
-    window.addEventListener("resize", handleResize);
-    window.addEventListener("pointermove", handleTilt);
-    handleResize();
+      handleResize();
+    }
 
     return () => {
       window.removeEventListener("resize", handleResize);
-      window.removeEventListener("pointermove", handleTilt);
     }
 
   })
 
   return (
     <div className="App">
-      <div ref={scaledContent}>
+      <div ref={scaledContent}
+        onMouseEnter={() => setLight(true)}
+        onMouseLeave={() => setLight(false)}
+        onMouseMove={(event) => handleTilt(event)}
+        onMouseOut={() => handleMouseOut()}
+      >
+
         {/* <FrontCard /> */}
-        <BackCard />
+        <BackCard  />
+        {light && (<span ref={lightComponent} className="light"></span>)}
       </div>
     </div>
   );
