1d1875ce9a7260e5c5ddac60e28055c06f2a478c
SamuelsonPajeu <samuelson.pajeu@gmail.com>
2023-11-20 23:44:17 -0300

    feat: added routing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as ReactDOM from "react-dom/client";
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from './App';
+import ErrorPage from './pages/Error/error-page';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <ErrorPage />,
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
